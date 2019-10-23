d68cf7948423a25aa9e9f5d003d7921e0e4fe951
JohnReiland <reiland.shanghai@gmail.com>
2019-10-23 18:55:21 -0400

    "Complete QUEUE for FUNCTIONAL pattern"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,30 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var storageKeys = Object.keys(storage);
+    if (storageKeys.length === 0) {
+      storage[0] = value;
+    } else {
+      var largest = Math.max(...storageKeys);
+      storage[largest+1] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    var storageKeys = Object.keys(storage);
+    if (storageKeys.length === 0) {
+      return undefined;
+    } else {
+      var smallest = Math.min(...storageKeys);
+      var hold=storage[smallest];
+      delete storage[smallest];
+      return hold;
+    }
   };
 
   someInstance.size = function() {
+    var storageKeys = Object.keys(storage);
+    return storageKeys.length;
   };
 
   return someInstance;
