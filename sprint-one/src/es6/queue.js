93fbaecc4ffeadaae9d3d57a88f76b64a797ca29
JohnReiland <reiland.shanghai@gmail.com>
2019-10-24 14:12:16 -0400

    "Complete STACK and QUEUE for ES6 pattern"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,36 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage={};
   }
 
+  enqueue (value) {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      this.storage[0] = value;
+    } else {
+      var largest = Math.max(...storageKeys);
+      this.storage[largest + 1] = value;
+    }
+  };
+
+  dequeue () {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      return undefined;
+    } else {
+      var smallest = Math.min(...storageKeys);
+      var hold = this.storage[smallest];
+      delete this.storage[smallest];
+      return hold;
+    }
+  };
+
+  size () {
+    var storageKeys = Object.keys(this.storage);
+    return storageKeys.length;
+  };
+
 }
+
+var newQueueStorage= new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,34 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
+  push(value) {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      this.storage[0] = value;
+    } else {
+      var largest = Math.max(...storageKeys);
+      this.storage[largest + 1] = value;
+    }
+  };
 
+  pop() {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      return undefined;
+    } else {
+      var largest = Math.max(...storageKeys);
+      var hold = this.storage[largest];
+      delete this.storage[largest];
+      return hold;
+    }
+  };
+
+  size() {
+    var storageKeys = Object.keys(this.storage);
+    return storageKeys.length;
+  };
 }
+
+var newStackStorage = new Stack();
