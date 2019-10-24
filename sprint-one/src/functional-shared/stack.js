27177529568fc52d24eaca732a052ba844a56cca
JohnReiland <reiland.shanghai@gmail.com>
2019-10-23 20:16:49 -0400

    "Preserve state at end of pair work for day 1"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not to reference your old code in writing the new style.
+  var someInstance = {};
+
+  // var storage = {};
+  // Object.assign(storage, stackMethods)
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+storage: {},
+push: function(value) {
+  var storageKeys = Object.keys(this.storage);
+  if (storageKeys.length === 0) {
+    this.storage[0] = value;
+  } else {
+    var largest = Math.max(...storageKeys);
+    this.storage[largest + 1] = value;
+  }
+},
+pop: function() {
+  var storageKeys = Object.keys(this.storage);
+  if (storageKeys.length === 0) {
+    return undefined;
+  } else {
+    var largest = Math.max(...storageKeys);
+    var hold = this.storage[largest];
+    delete this.storage[largest];
+    return hold;
+  }
+},
+size: function() {
+  var storageKeys = Object.keys(this.storage);
+  return storageKeys.length;
+}
 
-
+};
