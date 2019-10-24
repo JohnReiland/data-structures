19dd7825fdfef32b2e37a146ca7931ebc384a414
JohnReiland <reiland.shanghai@gmail.com>
2019-10-24 13:52:34 -0400

    "Complete STACK and QUEUE for PROTOTYPAL pattern"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -11,29 +11,28 @@ var Stack = function() {
 };
 
 var stackMethods = {
-push: function(value) {
-  var storageKeys = Object.keys(this.storage);
-  if (storageKeys.length === 0) {
-    this.storage[0] = value;
-  } else {
-    var largest = Math.max(...storageKeys);
-    this.storage[largest + 1] = value;
+  push: function(value) {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      this.storage[0] = value;
+    } else {
+      var largest = Math.max(...storageKeys);
+      this.storage[largest + 1] = value;
+    }
+  },
+  pop: function() {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      return undefined;
+    } else {
+      var largest = Math.max(...storageKeys);
+      var hold = this.storage[largest];
+      delete this.storage[largest];
+      return hold;
+    }
+  },
+  size: function() {
+    var storageKeys = Object.keys(this.storage);
+    return storageKeys.length;
   }
-},
-pop: function() {
-  var storageKeys = Object.keys(this.storage);
-  if (storageKeys.length === 0) {
-    return undefined;
-  } else {
-    var largest = Math.max(...storageKeys);
-    var hold = this.storage[largest];
-    delete this.storage[largest];
-    return hold;
-  }
-},
-size: function() {
-  var storageKeys = Object.keys(this.storage);
-  return storageKeys.length;
-}
-
 };
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var queueMethods = {};
+ queueMethods = {}
+
+  queueMethods.enqueue= function(value) {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      this.storage[0] = value;
+    } else {
+      var largest = Math.max(...storageKeys);
+      this.storage[largest + 1] = value;
+    }
+  };
+
+  queueMethods.dequeue= function() {
+    var storageKeys = Object.keys(this.storage);
+    if (storageKeys.length === 0) {
+      return undefined;
+    } else {
+      var smallest = Math.min(...storageKeys);
+      var hold = this.storage[smallest];
+      delete this.storage[smallest];
+      return hold;
+    }
+  };
+
+  queueMethods.size= function() {
+    var storageKeys = Object.keys(this.storage);
+    return storageKeys.length;
+  };
+
 
 
