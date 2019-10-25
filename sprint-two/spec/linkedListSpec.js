16e4ee500da7b06b9f62209212aa1d565f68e6dd
JohnReiland <reiland.shanghai@gmail.com>
2019-10-25 11:15:08 -0400

    "Add more methods and tests for linkedList"

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -51,11 +51,126 @@ describe('linkedList', function() {
     expect(linkedList.contains(4)).to.equal(false);
   });
 
+
   // add more tests here to test the functionality of linkedList
-  it('should designate a new head when a new node is added to an empty list', function() {
+
+  it('should designate a new head/tail when first node is added by calling addToTail', function() {
     linkedList.addToTail(4);
     expect(linkedList.head.value).to.equal(4);
-
+    expect(linkedList.tail.value).to.equal(4);
   });
+
+  it('should have a head that is also its tail when only one new node is added by calling addToTail', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.head).to.equal(linkedList.tail);
+  });
+
+
+
+  it('should designate a new head/tail when first node is added by calling addToHead', function() {
+    linkedList.addToHead(4);
+    expect(linkedList.head.value).to.equal(4);
+    expect(linkedList.tail.value).to.equal(4);
+  });
+
+  it('should have a head that is also its tail when only one new node is added by calling addToHead', function() {
+    linkedList.addToHead(4);
+    expect(linkedList.head.value).to.equal(4);
+    expect(linkedList.head).to.equal(linkedList.tail);
+  });
+
+  it('should designate a new head when addToHead is called after one or more nodes are added', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.head.value).to.equal(4);
+    linkedList.addToHead(3);
+    expect(linkedList.head.value).to.equal(3);
+  });
+
+
+
+  it('should remove the head/tail from the list when removeHead is called after only one node is added', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.head.value).to.equal(4);
+    expect(linkedList.tail.value).to.equal(4);
+    linkedList.removeHead();
+    expect(linkedList.head).to.equal(null);
+    expect(linkedList.tail).to.equal(null);
+  });
+
+
+
+  it('should remove the head/tail from the list when removeTail is called after only one node is added', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.head.value).to.equal(4);
+    expect(linkedList.tail.value).to.equal(4);
+    linkedList.removeTail();
+    expect(linkedList.head).to.equal(null);
+    expect(linkedList.tail).to.equal(null);
+  });
+
+  it('should remove the tail from the list when removeTail is called after more than one node is added', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.removeTail();
+    expect(linkedList.tail.value).to.equal(4);
+  });
+
+  it('should return the value of the former tail when removeTail is called', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.removeTail()).to.equal(4);
+  });
+
+  it('should return the value of the former tail when removeTail is called after more than one node is added', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.removeTail()).to.equal(5);
+  });
+
+
+
+  it('should return nth node when nthNode is called', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    expect(linkedList.nthNode(1)).to.equal(linkedList.head);
+    expect(linkedList.nthNode(3)).to.equal(linkedList.tail);
+  });
+
+  it('should return null when nthNode is called with n greater than number of nodes in list', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    expect(linkedList.nthNode(4)).to.equal(null);
+  });
+
+  it('should remove the nth node when removeNthNode is called', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    linkedList.removeNthNode(2);
+    expect(linkedList.contains(4)).to.equal(true);
+    expect(linkedList.contains(5)).to.equal(false);
+    expect(linkedList.contains(6)).to.equal(true);
+  });
+
+  it('should return the value of the former nth node when removeNthNode is called', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    expect(linkedList.removeNthNode(2)).to.equal(5);
+  });
+
+  it('should return the value of the nth node when nthNode.value is called', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToTail(6);
+    expect(linkedList.nthNode(2).value).to.equal(5);
+  });
+
+  // it('', function() {
+  //   linkedList.method();
+  //   expect().to.equal();
+  // });
 
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,25 +2,24 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  // list.hold;
- //list={head:null, tail:null}
-  list.addToTail = function(value) {
-  //  this.hold= new Node(value);
-    if(this.tail!==null){
-      this.tail.next=new Node(value);
-      this.tail=this.tail.next;
-    }
 
-    this.tail=new Node(value);
-    if(this.head===null){
-      this.head=this.tail;
+  list.addToTail = function(value) {
+    if (this.tail === null) {
+      this.tail = new Node(value);
+      this.head = this.tail;
+    } else {
+      this.tail.next = new Node(value);
+      this.tail = this.tail.next;
     }
   };
 
   list.removeHead = function() {
+    if (this.head === this.tail) {
+      this.tail = null;
+    }
     if (this.head !== null) {
       var removedNode = this.head;
-      this.head = removedNode.next;
+      this.head = this.head.next;
       return removedNode.value;
     }
   };
@@ -28,7 +27,6 @@ var LinkedList = function() {
   list.contains = function(target) {
     var currentNode = this.head;
     while (currentNode) {
-    console.log(currentNode);
       if (currentNode.value === target) {
         return true;
       }
@@ -36,6 +34,56 @@ var LinkedList = function() {
     };
     return false;
   }
+
+  // ---------------------------
+
+  list.removeTail = function() {
+    if (this.head === this.tail) {
+      this.tail = null;
+      return this.removeHead();
+    }
+    var currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.next === this.tail) {
+        var removedNode = this.tail;
+        this.tail = currentNode;
+        currentNode.next = null;
+        return removedNode.value;
+      }
+      currentNode = currentNode.next;
+    }
+  };
+
+  list.addToHead = function(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      var previousHead = this.head;
+      this.head = new Node(value);
+      this.head.next = previousHead;
+    }
+  };
+
+  list.nthNode = function(n) {
+    var currentNode = this.head;
+    for (var i = 1; currentNode && i < n; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  };
+
+  list.removeNthNode = function(n) {
+    var upstreamNode = this.nthNode(n-1);
+    if (upstreamNode === null) {
+      return this.removeHead();
+    } else {
+      var nthNode = upstreamNode.next;
+      upstreamNode.next = nthNode.next;
+      return nthNode.value;
+    }
+  };
+
 
   return list;
 };
@@ -53,3 +101,12 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// .addToTail is constant ------- (O)1
+// .removeHead is constant ------ (O)1
+// .contains is linear ---------- (O)n
+//
+// .addToHead is constant ------- (O)1
+// .removeTail is linear -------- (O)n
+// .nthNode is linear ----------- (O)n
+// .removeNthNode is linear ----- (O)n (because requires use of .nthNode, which is linear)
+//
