0756c40853b03a85bc06b80a69e27d397e38a182
JohnReiland <reiland.shanghai@gmail.com>
2019-10-26 00:20:45 -0400

    "Complete graph"

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,59 @@
 
 // Instantiate a new graph
 var Graph = function() {
-};
+   this.graphList={};
 
+};
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.graphList[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var objectNodes = Object.keys(this.graphList)
+  if (objectNodes.includes(node.toString())) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+
+  for (var i=0; i<this.graphList[node].length; i++){
+    this.removeEdge(node,this.graphList[node][i]);
+  }
+  delete this.graphList[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.graphList[fromNode].includes(toNode)) {
+    return true;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.graphList[fromNode].push(toNode);
+  this.graphList[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var fromNodeIndex = this.graphList[toNode].indexOf(fromNode);
+  var toNodeIndex = this.graphList[fromNode].indexOf(toNode);
+  this.graphList[toNode].splice(fromNodeIndex,1);
+  this.graphList[fromNode].splice(toNodeIndex,1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.graphList) {
+    cb(node.toString());
+  }
 };
 
 /*
@@ -37,3 +62,4 @@ Graph.prototype.forEachNode = function(cb) {
  */
 
 
+var ourFunnyGraph = new Graph();
